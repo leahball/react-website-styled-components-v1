1612215bc353b75803df89e2ed81705064bd03b5
iamleahb <leah.ball@gmail.com>
2022-02-14 11:44:40 -0600

    styles and content added

--- src/pages/portfolioPage/Octopus/index.js
+++ src/pages/portfolioPage/Octopus/index.js
@@ -1,47 +1,83 @@
 import React from "react";
+import Image1 from "./../../../images/strength-sm.png";
 import {
   PortfolioGrid,
-  PortfolioContent,
+  PortfolioMainContent,
+  PortfolioHeaderContent,
   PortfolioH1,
   PortfolioH2,
   PortfolioP,
   PortfolioSecondaryImages,
+  PortfolioImg,
   PortfolioListItems,
   PortfolioListH,
   PortfolioList,
+  PortfolioTextWrapper,
+  HeaderTextWrapper,
 } from "../PortfolioPageElements";
-
-import { Button } from "../../../components/ButtonElement";
 
 const Octopus = () => {
   return (
     <>
       <PortfolioGrid>
-        <PortfolioContent>
-          <PortfolioH1>Octopus Galaxy</PortfolioH1>
-          <PortfolioH2>
-            An interactive educational game about our fascinating underwater
-            neighbors!
-          </PortfolioH2>
-          <PortfolioP>
-            What makes me excited and optimistic is that everything on this
-            planet is made of the same stuff just arranged for us in our own
-            unique individual and species specific ways. While designing this
-            game I wanted that to both engage and comfort the player. Inspired
-            by the movie Arrival, a few amazing Octopus books, and my own
-            cyclical life processes, I wanted to spend time studying these many
-            armed friends while also learning React, Javascript and Redux.
-          </PortfolioP>
+        <PortfolioHeaderContent>
+          <HeaderTextWrapper>
+            <PortfolioH1>Octopus Galaxy</PortfolioH1>
+            <PortfolioH2>An interactive educational game about our</PortfolioH2>
+            <PortfolioH2>fascinating underwater neighbors!</PortfolioH2>
+          </HeaderTextWrapper>
+        </PortfolioHeaderContent>
+        <PortfolioMainContent>
           <PortfolioSecondaryImages>
-            <img src="" alt="" class="secondary-image" />
-            <img src="" alt="" class="secondary-image" />
-            <img src="" alt="" class="secondary-image" />
+            <PortfolioImg src={Image1} alt="illustration of an Octopus" />
           </PortfolioSecondaryImages>
+          <PortfolioTextWrapper>
+            <PortfolioP>
+              What makes me excited and optimistic is that everything on this
+              planet is made of the same stuff just arranged for us in our own
+              unique individual and species specific ways. For example--humans
+              have retinal cells behind our eyes, which are highly specialized
+              cells for visual perception, amazingly, Octopuses have them{" "}
+              <span className="highlight">ALL OVER</span> their skin! Take a
+              moment to imagine that? What possibilities does seeing with your
+              skin open up?{" "}
+              <span className="highlight">What does that even feel like?</span>{" "}
+              While designing this game I wanted to both engage and comfort the
+              player through wonder at the world around us. Inspired by the
+              movie Arrival, two amazing Octopus books,{" "}
+              <a
+                href="https://www.goodreads.com/book/show/28116739-other-minds"
+                target="_blank"
+                rel="noreferrer"
+              >
+                this one
+              </a>
+              , and{" "}
+              <a
+                href="https://www.goodreads.com/book/show/22609485-the-soul-of-an-octopus"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                this one
+              </a>
+              , and my own cyclical life processes, I wanted to spend time
+              studying these many armed friends while also learning{" "}
+              <span className="highlight">React, Javascript and Redux.</span>
+            </PortfolioP>
+            <PortfolioP>
+              While looking for coding challenges to practice I found a memory
+              game using simple vanilla javascript. While I had some very basic
+              experience with it, this challenge was my first time using xyz. I
+              used procreate to draw the first images and a for loop to create
+              the card board. There were some bugs in this first round:
+            </PortfolioP>
+          </PortfolioTextWrapper>
+
           <PortfolioListItems>
-            <PortfolioListH>Not sure what</PortfolioListH>
+            <PortfolioListH>Surprising Facts About Octopuses</PortfolioListH>
             <PortfolioList>
-              Here is a challenge I want to highlight and how I overcame it. It
-              wasn't easy but I did it.
+              The first game I built was in vanilla javascript, .
             </PortfolioList>
             <PortfolioList>
               Here is a challenge I want to highlight and how I overcame it. It
@@ -56,8 +92,7 @@ const Octopus = () => {
               wasn't easy but I did it.
             </PortfolioList>
           </PortfolioListItems>
-          <Button>Check it out here!</Button>
-        </PortfolioContent>
+        </PortfolioMainContent>
       </PortfolioGrid>
     </>
   );
--- src/pages/portfolioPage/PortfolioPageElements.js
+++ src/pages/portfolioPage/PortfolioPageElements.js
@@ -2,32 +2,76 @@ import styled from "styled-components";
 
 export const PortfolioGrid = styled.div`
   display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  gap: 20px;
   z-index: 1;
-  width: 100%;
-  max-width: 1100px;
-  margin-right: auto;
-  margin-left: auto;
-  padding: 3% 4%;
-  justify-content: center;
-  background-color: ${(p) => p.theme.lightMed};
-  border-radius: 20px;
+  padding: 0 4%;
+  background-color: ${(p) => p.theme.light};
 
   @media screen and (max-width: 768px) {
     padding: 5%;
   }
 `;
 
-export const PortfolioContent = styled.div`
-  padding: 3% 5%;
-  background: ${(p) => p.theme.light};
+export const PortfolioHeaderContent = styled.header`
+  display: grid;
+  grid-column: 1 / 7;
+  border-radius: 25px;
+  padding: 6% 6%;
+
+  /* display: flex;
+  align-items: center; */
+  background-image: url(${require(`../../images/octopus.png`)});
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: right;
+  background-color: ${(p) => p.theme.lightMed};
 
   @media screen and (max-width: 768px) {
     padding: 50px 20px;
   }
 `;
 
-export const PortfolioSecondaryImages = styled.img`
-  border: 1 px solid;
+export const PortfolioMainContent = styled.main`
+  display: grid;
+  grid-column: 1 / 7;
+  margin: 2%;
+
+  @media screen and (max-width: 768px) {
+    padding: 50px 20px;
+  }
+`;
+
+export const HeaderTextWrapper = styled.div`
+  width: 100%;
+  padding: 2% 3% 2% 3%;
+`;
+
+export const PortfolioTextWrapper = styled.div`
+  width: 100%;
+  padding: 2% 3% 2% 3%;
+  display: grid;
+  grid-column: 3 / 7;
+  gap: 20px;
+
+  p:first-child::first-letter {
+    float: left;
+    font-family: "Shrikhand", cursive;
+    font-size: 10rem;
+    line-height: 9rem;
+    padding-right: 20px;
+    padding-bottom: 0;
+    color: ${(p) => p.theme.contrast};
+  }
+`;
+
+export const PortfolioSecondaryImages = styled.div`
+  display: grid;
+  grid-column: 1 / 3;
+  max-width: 555px;
+`;
+
+export const PortfolioImg = styled.img`
   width: 100%;
   margin: 0 0 10px 0;
   padding-right: 0;
@@ -35,11 +79,12 @@ export const PortfolioSecondaryImages = styled.img`
 `;
 
 export const PortfolioH1 = styled.h1`
-  font-size: 4.5rem;
+  font-size: 7rem;
   font-family: "Shrikhand", cursive;
   letter-spacing: 0.1rem;
-  text-decoration-color: ${(p) => p.theme.melon};
-  margin-bottom: 36px;
+  color: ${(p) => p.theme.ink};
+  margin-bottom: 3%;
+  filter: drop-shadow(2px 2px 10px white);
 
   @media screen and (max-width: 480px) {
     font-size: 2.5rem;
@@ -47,9 +92,23 @@ export const PortfolioH1 = styled.h1`
 `;
 
 export const PortfolioH2 = styled.h2`
-  font-size: 2.5rem;
+  font-size: 3rem;
   font-family: "Poppins", sans-serif;
-  text-decoration-color: ${(p) => p.theme.ink};
+  color: ${(p) => p.theme.ink};
+  font-weight: 300;
+  position: relative;
+  z-index: 1;
+
+  :after {
+    content: "";
+    position: absolute;
+    width: 80%;
+    height: 70%;
+    background-color: ${(p) => p.theme.light};
+    left: -8px;
+    bottom: 6px;
+    z-index: -1;
+  }
 
   @media screen and (max-width: 480px) {
     font-size: 1.5rem;
@@ -57,33 +116,34 @@ export const PortfolioH2 = styled.h2`
 `;
 
 export const PortfolioP = styled.p`
-  max-width: 440px;
-  margin-bottom: 35px;
-  font-size: 2rem;
-  line-height: 3.4rem;
+  font-size: 1.6rem;
+  line-height: 2.6rem;
   color: ${(p) => p.theme.brown};
+  padding-bottom: 4%;
+
+  span.highlight {
+    background-color: ${(p) => p.theme.lightLogo};
+    font-style: italic;
+  }
+
+  a {
+    color: ${(p) => p.theme.contrast};
+  }
+  a:hover {
+    color: ${(p) => p.theme.lightLogo};
+    background-color: ${(p) => p.theme.ink};
+  }
 
   @media screen and (max-width: 480px) {
     margin-bottom: 0;
   }
 `;
 
-export const PortfolioListH = styled.h1`
-  margin-bottom: 16px;
-  color: ${(p) => p.theme.contrast};
-  font-size: 1.5rem;
-  font-weight: 300;
-  text-transform: uppercase;
-`;
-
 export const PortfolioListItems = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin: 16px;
-  text-align: left;
-  width: 180px;
+  display: grid;
+  grid-column: 1 / 4;
   box-sizing: border-box;
+  padding: 6%;
 
   @media screen and (max-width: 420px) {
     margin: 0;
@@ -92,25 +152,19 @@ export const PortfolioListItems = styled.div`
   }
 `;
 
+export const PortfolioListH = styled.h3`
+  margin-bottom: 16px;
+  color: ${(p) => p.theme.contrast};
+  font-size: 2rem;
+  font-weight: 400;
+  text-decoration: wavy;
+  text-decoration-color: ${(p) => p.theme.contrast};
+`;
+
 export const PortfolioList = styled.li`
   color: ${(p) => p.theme.brown};
   text-decoration: none;
   margin-bottom: 8%;
   font-size: 1.5rem;
-  line-height: 4rem;
-  position: relative;
-  z-index: 1;
-  text-decoration: overline;
-  text-decoration-color: ${(p) => p.theme.melon};
-
-  :after {
-    content: "";
-    position: absolute;
-    width: 80%;
-    height: 50%;
-    background-color: ${(p) => p.theme.light};
-    left: -8px;
-    bottom: 6px;
-    z-index: -1;
-  }
+  line-height: 3rem;
 `;
